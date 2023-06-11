896e54a6710a556d166c45add1aa483d39d7802d
Valivia <valivia@xayania.com>
2023-06-11 21:03:59 +0000

    🧹 Added unit and quantity

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -1,6 +1,95 @@
 const { PrismaClient } = require("@prisma/client");
 const dataset = require("./source/dataset.json");
 
+function parseQuantityAndUnit(inputString) {
+    inputString = inputString.slice(3);
+    const quantity = parseFloat(inputString.replace(/[^0-9]/g, ""));
+    const unit = inputString.replace(/[0-9.]/g, "").trim();
+
+    return { quantity, unit };
+}
+
+function processNutrients(entry) {
+    const nutrients = {
+        ENERCC: entry["ENERCC (kcal)"],
+        FAT: entry["FAT (g)"],
+        PROT: entry["PROT (g)"],
+        PROTPL: entry["PROTPL (g)"],
+        PROTAN: entry["PROTAN (g)"],
+        CHO: entry["CHO (g)"],
+        FIBT: entry["FIBT (g)"],
+        FAPUN3: entry["FAPUN3 (g)"],
+        FAPUN6: entry["FAPUN6 (g)"],
+        F18_3CN3: entry["F18:3CN3 (g)"],
+        F18_4CN3: entry["F18:4CN3 (g)"],
+        F20_3CN3: entry["F20:3CN3 (g)"],
+        F20_4CN3: entry["F20:4CN3 (g)"],
+        F20_5CN3: entry["F20:5CN3 (g)"],
+        F21_5CN3: entry["F21:5CN3 (g)"],
+        F22_2CN3: entry["F22:2CN3 (g)"],
+        F22_3CN3: entry["F22:3CN3 (g)"],
+        F22_5CN3: entry["F22:5CN3 (g)"],
+        F22_6CN3: entry["F22:6CN3 (g)"],
+        F18_2CN6: entry["F18:2CN6 (g)"],
+        F18_3CN6: entry["F18:3CN6 (g)"],
+        F20_2CN6: entry["F20:2CN6 (g)"],
+        F20_3CN6: entry["F20:3CN6 (g)"],
+        F20_4CN6: entry["F20:4CN6 (g)"],
+        F22_2CN6: entry["F22:2CN6 (g)"],
+        F22_4CN6: entry["F22:4CN6 (g)"],
+        F22_5CN6: entry["F22:5CN6 (g)"],
+        F24_2CN6: entry["F24:2CN6 (g)"],
+        MG: entry["MG (mg)"],
+        FE: entry["FE (mg)"],
+        HAEM: entry["HAEM (mg)"],
+        NHAEM: entry["NHAEM (mg)"],
+        SE: entry["SE (µg)"],
+        ZN: entry["ZN (mg)"],
+        ID: entry["ID (µg)"],
+        VITD: entry["VITD (µg)"],
+        CHOCALOH: entry["CHOCALOH (µg)"],
+        CHOCAL: entry["CHOCAL (µg)"],
+        THIA: entry["THIA (mg)"],
+        RIBF: entry["RIBF (mg)"],
+        VITB6: entry["VITB6 (mg)"],
+        VITB12: entry["VITB12 (µg)"],
+        NIA: entry["NIA (mg)"],
+    };
+
+    // Sanitize
+    for (const key in nutrients) {
+        const value = nutrients[key];
+
+        if (value === undefined) {
+            console.warn(`Missing value for ${key} in ${entry["NEVO-code"]}`);
+            continue;
+        }
+
+        if (value === "NULL") {
+            nutrients[key] = 0;
+            continue;
+        }
+
+        if (typeof value === "string") {
+            nutrients[key] = parseFloat(value.replace(",", "."));
+
+            if (isNaN(nutrients[key])) {
+                console.warn(`Invalid value for ${key} in ${entry["NEVO-code"]}`);
+                nutrients[key] = null;
+                return;
+            }
+            continue;
+        }
+
+        if (typeof value === "number") {
+            continue;
+        }
+
+
+        console.warn(`Invalid value for ${key} in ${entry["NEVO-code"]}`);
+        return;
+    }
+}
 
 async function main() {
     const prisma = new PrismaClient();
@@ -12,86 +101,8 @@ async function main() {
     for (const entry of dataset) {
         count++;
 
-        const nutrients = {
-            ENERCC: entry["ENERCC (kcal)"],
-            FAT: entry["FAT (g)"],
-            PROT: entry["PROT (g)"],
-            PROTPL: entry["PROTPL (g)"],
-            PROTAN: entry["PROTAN (g)"],
-            CHO: entry["CHO (g)"],
-            FIBT: entry["FIBT (g)"],
-            FAPUN3: entry["FAPUN3 (g)"],
-            FAPUN6: entry["FAPUN6 (g)"],
-            F18_3CN3: entry["F18:3CN3 (g)"],
-            F18_4CN3: entry["F18:4CN3 (g)"],
-            F20_3CN3: entry["F20:3CN3 (g)"],
-            F20_4CN3: entry["F20:4CN3 (g)"],
-            F20_5CN3: entry["F20:5CN3 (g)"],
-            F21_5CN3: entry["F21:5CN3 (g)"],
-            F22_2CN3: entry["F22:2CN3 (g)"],
-            F22_3CN3: entry["F22:3CN3 (g)"],
-            F22_5CN3: entry["F22:5CN3 (g)"],
-            F22_6CN3: entry["F22:6CN3 (g)"],
-            F18_2CN6: entry["F18:2CN6 (g)"],
-            F18_3CN6: entry["F18:3CN6 (g)"],
-            F20_2CN6: entry["F20:2CN6 (g)"],
-            F20_3CN6: entry["F20:3CN6 (g)"],
-            F20_4CN6: entry["F20:4CN6 (g)"],
-            F22_2CN6: entry["F22:2CN6 (g)"],
-            F22_4CN6: entry["F22:4CN6 (g)"],
-            F22_5CN6: entry["F22:5CN6 (g)"],
-            F24_2CN6: entry["F24:2CN6 (g)"],
-            MG: entry["MG (mg)"],
-            FE: entry["FE (mg)"],
-            HAEM: entry["HAEM (mg)"],
-            NHAEM: entry["NHAEM (mg)"],
-            SE: entry["SE (µg)"],
-            ZN: entry["ZN (mg)"],
-            ID: entry["ID (µg)"],
-            VITD: entry["VITD (µg)"],
-            CHOCALOH: entry["CHOCALOH (µg)"],
-            CHOCAL: entry["CHOCAL (µg)"],
-            THIA: entry["THIA (mg)"],
-            RIBF: entry["RIBF (mg)"],
-            VITB6: entry["VITB6 (mg)"],
-            VITB12: entry["VITB12 (µg)"],
-            NIA: entry["NIA (mg)"],
-        };
-
-        // Sanitize
-        for (const key in nutrients) {
-            const value = nutrients[key];
-
-            if (value === undefined) {
-                console.warn(`Missing value for ${key} in ${entry["NEVO-code"]}`);
-                continue;
-            }
-
-            if (value === "NULL") {
-                nutrients[key] = 0;
-                continue;
-            }
-
-            if (typeof value === "string") {
-                nutrients[key] = parseFloat(value.replace(",", "."));
-
-                if (isNaN(nutrients[key])) {
-                    console.warn(`Invalid value for ${key} in ${entry["NEVO-code"]}`);
-                    nutrients[key] = null;
-                    return;
-                }
-                continue;
-            }
-
-            if (typeof value === "number") {
-                continue;
-            }
-
-
-            console.warn(`Invalid value for ${key} in ${entry["NEVO-code"]}`);
-            return;
-        }
-
+        const nutrients = processNutrients(entry);
+        const { quantity, unit } = parseQuantityAndUnit(entry["Hoeveelheid/Quantity"]);
         promises.push(
             prisma.ingredient.create({
                 data: {
@@ -100,6 +111,8 @@ async function main() {
                     name_nl: entry["Voedingsmiddelnaam/Dutch food name"],
                     category: entry["Food group"],
                     category_nl: entry["Voedingsmiddelgroep"],
+                    quantity,
+                    unit,
                     nutrients: {
                         create: nutrients
                     }
