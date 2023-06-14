4569fdb5f6fa1bad17366f8dfe310b5859242bf7
sophiavl <102966872+sophiavl@users.noreply.github.com>
2023-06-14 12:57:25 +0200

    ringen en fonts gemaakt

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import Ring from '../components/ProgressRing.js'
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <section className='home'>
+      <section id="homepageDagelijksDoel">
+      <Ring progress={0.50} radius={30} strokeWidth={5}/>
+      <section>
+        <h1>Dagelijks doel</h1>
+        <p>21 Kcal</p>
+      </section>
+    </section>
+    <section id='ringenVerschillendeVoedingsw'>
+      <Ring progress={0.5} radius={30} strokeWidth={8}></Ring>
+      <Ring progress={0.5} radius={30} strokeWidth={8}></Ring>
+      <Ring progress={0.5} radius={30} strokeWidth={8}></Ring>
+
+    </section>
+    </section>
+    
+    
   )
 }
 
