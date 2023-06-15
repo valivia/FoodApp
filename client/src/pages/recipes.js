9163594bd7fdbfcc19e127de697104be45129a5f
sophiavl <102966872+sophiavl@users.noreply.github.com>
2023-06-15 22:43:21 +0200

    added icon to recipe header and improved classNames

--- client/src/pages/recipes.js
+++ client/src/pages/recipes.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import styles from './recipes.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'  
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 
 function RecipePage() {
   return (
@@ -7,55 +9,29 @@ function RecipePage() {
       <section className={styles.header}>
         <h1>Recipes</h1>
       </section>
-      <section className={styles.boxes}>
-        <section className={styles.box}>
 
-          <section className={styles.boxHeader}>
-            <h3>Lasagne</h3>
-            <p>800kcal</p>
+      <section className={styles.recipesWrapper}>
+
+        <section className={styles.recipe}>
+          <section className={styles.recipeHeader}>
+            <section className={styles.textWrapper}>
+              <h3>Lasagne</h3>
+              <p>800kcal</p>
+            </section>
+            <section className={styles.iconWrapper}>
+              <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+            </section>
           </section>
-
-          <section className={styles.lineBox}>
+          
+          <section className={styles.lineWrapper}>
             <div className={styles.line}></div>
           </section>
 
           <section className={styles.description}>
             <p>nomnomnom</p>
           </section>
-
         </section>
-        <section className={styles.box}>
 
-          <section className={styles.boxHeader}>
-            <h3>Lasagne</h3>
-            <p>800kcal</p>
-          </section>
-
-          <section className={styles.lineBox}>
-            <div className={styles.line}></div>
-          </section>
-
-          <section className={styles.description}>
-            <p>nomnomnom</p>
-          </section>
-
-        </section>
-        <section className={styles.box}>
-
-          <section className={styles.boxHeader}>
-            <h3>Lasagne</h3>
-            <p>800kcal</p>
-          </section>
-
-          <section className={styles.lineBox}>
-            <div className={styles.line}></div>
-          </section>
-
-          <section className={styles.description}>
-            <p>nomnomnom</p>
-          </section>
-
-        </section>
       </section>
     </main>
   )
