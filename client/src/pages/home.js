8a3384c340e1eb31867bd33605288da656219552
sophiavl <102966872+sophiavl@users.noreply.github.com>
2023-06-15 16:13:16 +0200

    positioning rings and dark background for daily goal

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -7,17 +7,18 @@ function HomePage() {
     <main className={styles.home}>
 
       <section className={styles.dailygoal}>
-        <Dial progress={0.2} size={20} />
-        <section>
+      <section>
           <h1>Daily Goal</h1>
-          <p>21 Kcal</p>
+          <p>2100 Kcal</p>
         </section>
+        <Dial progress={0.2} size={8} />
+        
       </section>
 
       <section className={styles.dials}>
-        <Dial progress={0.5} size={10}></Dial>
-        <Dial progress={0.7} size={10}></Dial>
-        <Dial progress={0.89} size={10}></Dial>
+        <Dial progress={0.5} size={5}></Dial>
+        <Dial progress={0.7} size={5}></Dial>
+        <Dial progress={0.89} size={5}></Dial>
       </section>
 
     </main>
