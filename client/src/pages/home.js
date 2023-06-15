bd252a19b8a95e0bc8e86d3b1aea751f33f1d2a9
sophiavl <102966872+sophiavl@users.noreply.github.com>
2023-06-15 16:46:07 +0200

    changed background of dials in homepage

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -11,21 +11,21 @@ function HomePage() {
           <h1>Daily Goal</h1>
           <p>2100 Kcal</p>
         </section>
-        <Dial progress={0.2} size={8} />
+        <Dial progress={0.2} size={8} backgroundColor={'#191B19'}/>
         
       </section>
 
       <section className={styles.dials}>
         <section className={styles.dial}>        
-          <Dial progress={0.5} size={5}></Dial>
+          <Dial progress={0.5} size={5} backgroundColor={'#292B27'}></Dial>
           <p>Protein</p>
         </section>
         <section className={styles.dial}>        
-          <Dial progress={0.7} size={5}></Dial>
+          <Dial progress={0.7} size={5} backgroundColor={'#292B27'}></Dial>
           <p>Fiber</p>
         </section>
         <section className={styles.dial}>        
-          <Dial progress={0.89} size={5}></Dial>
+          <Dial progress={0.89} size={5} backgroundColor={'#292B27'}></Dial>
           <p>Fat</p>
         </section>
       </section>
