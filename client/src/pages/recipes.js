76146322997ebdb302f258ae3786d17640804f7f
sophiavl <102966872+sophiavl@users.noreply.github.com>
2023-06-15 21:20:28 +0200

    made recipe layout and recipeboxes

--- client/src/pages/recipes.js
+++ client/src/pages/recipes.js
@@ -3,7 +3,61 @@ import styles from './recipes.module.css'
 
 function RecipePage() {
   return (
-    <div>Recipes</div>
+    <main className={styles.recipes}>
+      <section className={styles.header}>
+        <h1>Recipes</h1>
+      </section>
+      <section className={styles.boxes}>
+        <section className={styles.box}>
+
+          <section className={styles.boxHeader}>
+            <h3>Lasagne</h3>
+            <p>800kcal</p>
+          </section>
+
+          <section className={styles.lineBox}>
+            <div className={styles.line}></div>
+          </section>
+
+          <section className={styles.description}>
+            <p>nomnomnom</p>
+          </section>
+
+        </section>
+        <section className={styles.box}>
+
+          <section className={styles.boxHeader}>
+            <h3>Lasagne</h3>
+            <p>800kcal</p>
+          </section>
+
+          <section className={styles.lineBox}>
+            <div className={styles.line}></div>
+          </section>
+
+          <section className={styles.description}>
+            <p>nomnomnom</p>
+          </section>
+
+        </section>
+        <section className={styles.box}>
+
+          <section className={styles.boxHeader}>
+            <h3>Lasagne</h3>
+            <p>800kcal</p>
+          </section>
+
+          <section className={styles.lineBox}>
+            <div className={styles.line}></div>
+          </section>
+
+          <section className={styles.description}>
+            <p>nomnomnom</p>
+          </section>
+
+        </section>
+      </section>
+    </main>
   )
 }
 
