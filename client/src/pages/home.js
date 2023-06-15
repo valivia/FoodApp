9199d4a0f38179f14a79058dd2d20cd59cdffeb2
Valivia <valivia@xayania.com>
2023-06-15 18:47:28 +0000

    replaced hex codes with variables

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -11,21 +11,21 @@ function HomePage() {
           <h1>Daily Goal</h1>
           <p>2100 Kcal</p>
         </section>
-        <Dial progress={0.2} size={8} backgroundColor={'#191B19'}/>
+        <Dial progress={0.2} size={8} backgroundColor={"var(--secondary)"}/>
         
       </section>
 
       <section className={styles.dials}>
         <section className={styles.dial}>        
-          <Dial progress={0.5} size={5} backgroundColor={'#292B27'}></Dial>
+          <Dial progress={0.5} size={5} backgroundColor={"var(--primary)"}></Dial>
           <p>Protein</p>
         </section>
         <section className={styles.dial}>        
-          <Dial progress={0.7} size={5} backgroundColor={'#292B27'}></Dial>
+          <Dial progress={0.7} size={5} backgroundColor={"var(--primary)"}></Dial>
           <p>Fiber</p>
         </section>
         <section className={styles.dial}>        
-          <Dial progress={0.89} size={5} backgroundColor={'#292B27'}></Dial>
+          <Dial progress={0.89} size={5} backgroundColor={"var(--primary)"}></Dial>
           <p>Fat</p>
         </section>
       </section>
