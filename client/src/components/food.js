751b840fa68034bb45ab4d4f36f1482f86e8a8bd
sophiavl <102966872+sophiavl@users.noreply.github.com>
2023-06-17 20:48:42 +0200

    made fontsize of foodName smaller

--- client/src/components/food.js
+++ client/src/components/food.js
@@ -4,9 +4,12 @@ const Food = ({foodName, protein, fiber, fat}) => {
     return (
         <div className={styles.foodContainer}>
             <h3>{foodName}</h3>
+            <section className={styles.nutrionalValues}>
             <p className={styles.protein}>{protein}%</p>
             <p className={styles.fiber}>{fiber}%</p>
             <p className={styles.fat}>{fat}%</p>
+            </section>
+            
         </div>
     )
 }
--- client/src/pages/diary.js
+++ client/src/pages/diary.js
@@ -11,7 +11,7 @@ function DiaryPage() {
         <h2>Breakfast</h2>
         <Food foodName={'Egg'} protein={7.8} fiber={0} fat={90}></Food>
         <h2>Lunch</h2>
-        <Food foodName={'Yoghurt with oats'} protein={10} fiber={10} fat={4}></Food>
+        <Food foodName={'Yoghurt'} protein={10} fiber={10} fat={4}></Food>
         <h2>Diner</h2>
         <Food foodName={'Rice'} protein={4} fiber={20} fat={0}></Food>
         <Food foodName={'Chicken'} protein={30} fiber={0} fat={20}></Food>
