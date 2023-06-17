64b5673042d1a95a95b2a73e30b5574b6e4b38aa
Valivia <valivia@xayania.com>
2023-06-17 22:26:01 +0000

    🧹 Cleanup

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from "./app.service";
 import { IngredientModule } from "./ingredient/ingredient.module";
 import { RecipeModule } from "./recipe/recipe.module";
 import { DiaryModule } from "./diary/diary.module";
+import { UserModule } from "./user/user.module";
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
-  imports: [IngredientModule, RecipeModule, DiaryModule],
+  imports: [IngredientModule, RecipeModule, DiaryModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
