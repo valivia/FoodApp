fc6e0648a473c862f12a9269cdc1cbf3b6f5e0ad
sophiavl <102966872+sophiavl@users.noreply.github.com>
2023-06-19 17:14:49 +0200

    made the styling for the profile page and added a avatar picture, didnt work so made a dummy circle avatar

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -3,23 +3,32 @@ import styles from '../pages/profile.module.css';
 function ProfilePage () {
 const firstName = 'Sophia'
 const lastName = 'van Lieshout'
+const fullName = firstName + ' ' + lastName
+const emailAddress = 'sophiavl300@gmail.com'
+const height = 180;
+const weight = 63;
 
     return (
         <section className={styles.profilePage}>
+            <div className={styles.avatar}></div>
+            {/* <img src='src/assets/avatar.jpg' alt='avatar'></img> */}
             <h1>{firstName} {lastName}</h1>
             <form className={styles.profileForm}>
                 <h2>Account</h2>
                 <label>Name</label>
-                <input></input>
+                <input type='text' value={fullName}></input>
                 <label>Email adress</label>
-                <input></input>
+                <input type='text' value={emailAddress}></input>
 
                 <h2>Measurements</h2>
                 <label>Height</label>
-                <input></input>
+                <input type='number' value={height}></input>
                 <label>Weight</label>
-                <input></input>
+                <input type='number' value={weight}></input>
+
+                <section className={styles.buttonWrapper}>
                 <button type='submit'>Save</button>
+                </section>
             </form>
         </section>
     )
