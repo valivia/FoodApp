304da00e009a29c97591d8736578becef96d045a
sophiavl <102966872+sophiavl@users.noreply.github.com>
2023-06-19 12:05:20 +0200

    Made register.js and register.module.css

---
+++ client/src/pages/register.js
@@ -0,0 +1,11 @@
+import styles from '../pages/register.module.css';
+
+function RegisterPage () {
+    return (
+        <section className={styles.registerpage}>
+
+        </section>
+    )
+}
+
+export default RegisterPage
