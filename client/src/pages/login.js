73483bce82d3bd82e5a30277fbbe0c4f4479f7a4
sophiavl <102966872+sophiavl@users.noreply.github.com>
2023-06-19 12:57:51 +0200

    made base for login page

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,6 +1,19 @@
+import styles from '../pages/login.module.css';
+
 function LoginPage () {
     return (
-        <section></section>
+        <section className={styles.loginPage}>
+            <h2>Log In</h2>
+            <form className={styles.loginForm}>
+                <label for="username">Username:</label>
+                <input type='text' id='username' name='username' required/>
+                <label for="password">Password:</label>
+                <input type='password' id='password' name='password' required/>
+            </form>
+            <section className={styles.buttonWrapper}>
+                <button type='submit'><p>Register</p></button>
+                </section>
+        </section>
     )
 }
 
